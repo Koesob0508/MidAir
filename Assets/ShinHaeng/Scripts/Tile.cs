d9f2eb97ab15de9cee25b7fb632d7407e362ab91
sittan2 <hang5050@naver.com>
2022-06-04 23:14:34 +0900

    하늘섬이 최초 생성 시 자식으로 타일을 생성함 (count = 63) 만큼.

--- Assets/ShinHaeng/Scripts/Tile.cs
+++ Assets/ShinHaeng/Scripts/Tile.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum eType
     {
-        
+        none = 0, 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
