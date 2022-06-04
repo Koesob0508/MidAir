7f97141db5b7295ff5c84fdbabac93605edf2605
sittan2 <hang5050@naver.com>
2022-06-05 05:12:50 +0900

    TileSet 생성

---
+++ Assets/ShinHaeng/Scripts/TileSet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.ShinHaeng.Scripts
+{
+    public class TileSet : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
