acaa09ffa75de38c2f8c685208707b801968e608
sittan2 <hang5050@naver.com>
2022-06-04 22:39:42 +0900

    InputManager, Tile commit

---
+++ Assets/ShinHaeng/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    UnityAction inputAction;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
