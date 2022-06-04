30452f9e0beea30ed0a397197505add6e39a9890
sittan2 <hang5050@naver.com>
2022-06-05 01:08:33 +0900

    tag 추가

--- Assets/ShinHaeng/Scripts/InputManager.cs
+++ Assets/ShinHaeng/Scripts/InputManager.cs
@@ -7,22 +7,30 @@ public class InputManager : MonoBehaviour
     //UnityAction inputAction;
     [SerializeField] Camera _camera;
     [SerializeField] Island Island;
-    [SerializeField] int skyMask = 10;
+    [SerializeField] int skyLayerMask;
+    [SerializeField] int objectLayerMask;
 
     private void Awake()
     {
         this._camera = Camera.main;
         Island = GetComponent<Island>();
+        skyLayerMask = 1 << LayerMask.NameToLayer("Sky");
+        objectLayerMask = 1 << LayerMask.NameToLayer("Build");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(-1))
+        if (Input.GetMouseButton(1))
         {
-            //RaycastHit raycastHit = Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition),
-            //    out RaycastHit hit, skyMask);
-            //Island.Move(Input.mousePosition);
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, skyLayerMask)) ;
+                //Island.Move(hit);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, objectLayerMask))
+                Debug.Log(hit.collider.GetComponent<Building>().name);
         }
     }
 }
